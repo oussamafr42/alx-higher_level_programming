a66ea7e2d6e7465ef59aef46bcc885544a9406f3
oussamafr42 <faroukoussama42@gmail.com>
2024-02-10 13:28:00 -0800

    ALL 2

---
+++ 0x03-python-data_structures/100-test_lists.py
@@ -0,0 +1,25 @@
+79 % de l'espace de stockage utilisés … 
+Une fois la limite atteinte, vous n'aurez plus assez d'espace pour créer, modifier ou importer des fichiers. Profitez de 100 Go de stockage pour 19,00 MAD 4,75 MAD/mois pendant 3 mois.
+100-test_lists.py
+
+import ctypes
+
+lib = ctypes.CDLL('./libPyList.so')
+lib.print_python_list_info.argtypes = [ctypes.py_object]
+l = ['hello', 'World']
+lib.print_python_list_info(l)
+del l[1]
+lib.print_python_list_info(l)
+l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "Holberton"]
+lib.print_python_list_info(l)
+l = []
+lib.print_python_list_info(l)
+l.append(0)
+lib.print_python_list_info(l)
+l.append(1)
+l.append(2)
+l.append(3)
+l.append(4)
+lib.print_python_list_info(l)
+l.pop()
+lib.print_python_list_info(l)
